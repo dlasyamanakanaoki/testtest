1d5805f3fb5321d8f1e47769ea4be54f23e04498
n_yamanaka <n_yamanaka@dlas.jp>
2023-10-19 15:11:57 +0900

    test

--- dlastest/test.py
+++ dlastest/test.py
@@ -12,3 +12,17 @@ def aaa(a, b):
     """
 
     return []
+
+
+def bbb(a, b):
+    """_summary_
+
+    Args:
+        a (str): aaa
+        b (str): bbb
+
+    Returns:
+        list: list
+    """
+
+    return []
